1c6dcdc2be487e6601046814ef067320bf523a97
Khushi <109527041+khushi-upadhyay@users.noreply.github.com>
2026-09-26 21:36:07 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
@@ -0,0 +1,36 @@
+//This is generally known as "Monotonic increasing/decreasing  Queue/Dequeue"
+//Approach-1 (Using Deque) Every element is added(pushed) and popped only once,So it is O(n) time complexity.
+
+// Story Points:
+// 1. When a new element comes nums[i], make space for it (window size can't be greater than k).
+// 2. When nums[i] comes, there is no need to keep smaller elements in that window → pop them.
+// 3. Push i into the deque → for nums[i].
+// 4. If i >= k - 1, then deque.front() is our answer for that window.
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n == 0)
+            return {};
+        deque<int> deq;
+
+        vector<int> result;
+        
+        for(int i = 0; i<n; i++) {
+            //remove the max elements from front which are out of window size
+            while(!deq.empty() && deq.front() <= i-k)
+                deq.pop_front();
+            
+            //we maintain the deque in descending order
+            while(!deq.empty() && nums[i] > nums[deq.back()])
+                deq.pop_back();
+            
+            deq.push_back(i);
+
+            if(i >= k-1) //Only when the window size first gets equal or greater than k
+                result.push_back(nums[deq.front()]); //front will have the max element (dequeue is maintained in descending order)
+        }
+        return result;
+    }
+};
